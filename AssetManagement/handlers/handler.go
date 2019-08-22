f8370f4210bfe823179cbb6ba4fac2744f3b61a1
adhimas waskita ramadhana <adhimaswaskita7a@gmail.com>
2019-08-22 11:49:12 +0700

    Finish handler create manufacture

--- AssetManagement/handlers/handler.go
+++ AssetManagement/handlers/handler.go
@@ -8,10 +8,13 @@ import (
 
 //IHandler is handler contract
 type IHandler interface {
+	//Product Type
 	CreateProductType(w http.ResponseWriter, r *http.Request)
 	GetAllProductType(w http.ResponseWriter, r *http.Request)
 	UpdateProductType(w http.ResponseWriter, r *http.Request)
 	DeleteProductType(w http.ResponseWriter, r *http.Request)
+	//Manufacture
+	CreateManufacture(w http.ResponseWriter, r *http.Request)
 }
 
 //Handler is http request handler that implements IHandler
