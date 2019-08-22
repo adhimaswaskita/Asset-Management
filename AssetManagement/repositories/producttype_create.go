7324cd66325e77074567a8365f5630c759897ddb
adhimas waskita ramadhana <adhimaswaskita7a@gmail.com>
2019-08-22 13:35:58 +0700

    fix create product type auto migrate

--- AssetManagement/repositories/producttype_create.go
+++ AssetManagement/repositories/producttype_create.go
@@ -4,6 +4,7 @@ import nmodel "github.com/adhimaswaskita/AssetManagement/models"
 
 //CreateProductType insert data to database from param
 func (r *Repository) CreateProductType(pt *nmodel.ProductType) (*nmodel.ProductType, error) {
+	r.DB.AutoMigrate(&pt)
 	r.DB.Create(&pt)
 	return pt, nil
 }
