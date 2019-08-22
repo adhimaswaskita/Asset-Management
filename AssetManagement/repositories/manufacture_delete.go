0fdc099832c9be195ab63008b922f3912060a654
adhimas waskita ramadhana <adhimaswaskita7a@gmail.com>
2019-08-22 16:28:07 +0700

    add update and delete manufacture endpoint

--- AssetManagement/repositories/manufacture_delete.go
+++ AssetManagement/repositories/manufacture_delete.go
@@ -1,6 +1,11 @@
 package repositories
 
+import nmodel "github.com/adhimaswaskita/AssetManagement/models"
+
 //DeleteManufacture is used to delete manufacture data from database
 func (r *Repository) DeleteManufacture(ID uint) error {
+	manufacture := nmodel.Manufacture{}
+	r.DB.Where("ID = ?", ID).Delete(&manufacture)
+
 	return nil
 }
