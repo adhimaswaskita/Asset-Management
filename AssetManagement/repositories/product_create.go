dd206f96189ea2e8d2ae68637c9aafa0402ed46c
adhimaswaskita <adhimas.waskita.r@gmail.com>
2019-08-26 15:34:47 +0700

    error handling when no foreign key

--- AssetManagement/repositories/product_create.go
+++ AssetManagement/repositories/product_create.go
@@ -4,7 +4,10 @@ import nmodel "github.com/adhimaswaskita/AssetManagement/models"
 
 //CreateProduct is used to insert manufacture data to database
 func (r *Repository) CreateProduct(product *nmodel.Product) (*nmodel.Product, error) {
-	r.DB.Create(&product)
+	err := r.DB.Create(&product).Error
+	if err != nil {
+		return nil, err
+	}
 
 	return product, nil
 }
