36b2fe819ce6c4870fdb09d302e9702976e167bb
adhimaswaskita <adhimas.waskita.r@gmail.com>
2019-08-26 10:09:30 +0700

    Finish Handlers-CRUD productsupplier

---
+++ AssetManagement/handlers/productsupplier_delete.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	nrf "github.com/adhimaswaskita/AssetManagement/lib/responseformat"
+	nmodel "github.com/adhimaswaskita/AssetManagement/models"
+	"github.com/gorilla/mux"
+)
+
+//DeleteProductSupplier is used to delete product supplier by it's ID
+func (h *Handler) DeleteProductSupplier(w http.ResponseWriter, r *http.Request) {
+	rf := nrf.ResponseFormat{}
+	productSupplierParam := &nmodel.ProductSupplier{}
+
+	params := mux.Vars(r)
+	id := params["id"]
+
+	intID, _ := strconv.Atoi(id)
+	err := h.Service.DeleteProductSupplier(intID)
+	if err != nil {
+		rf.Response(nrf.ERROR, productSupplierParam, w)
+		return
+	}
+
+	rf.Response(nrf.SUCCESS, productSupplierParam, w)
+}
