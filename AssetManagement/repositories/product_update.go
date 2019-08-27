55a70826bf2cb3dcc4c5be30a1e2a065deda8ec1
adhimaswaskita <adhimas.waskita.r@gmail.com>
2019-08-27 09:50:29 +0700

    error given when no constraints

--- AssetManagement/repositories/product_update.go
+++ AssetManagement/repositories/product_update.go
@@ -1,11 +1,16 @@
 package repositories
 
-import nmodel "github.com/adhimaswaskita/AssetManagement/models"
+import (
+	nmodel "github.com/adhimaswaskita/AssetManagement/models"
+)
 
 //UpdateProduct is used to save updated data to database
 func (r *Repository) UpdateProduct(ID uint, Product *nmodel.Product) (*nmodel.Product, error) {
 	var productParam nmodel.Product
 
-	r.DB.Model(&productParam).Where("ID = ?", ID).Updates(Product)
+	err := r.DB.Model(&productParam).Where("ID = ?", ID).Updates(Product).Error
+	if err != nil {
+		return nil, err
+	}
 	return Product, nil
 }
--- AssetManagement/repositories/product_update_test.go
+++ AssetManagement/repositories/product_update_test.go
@@ -1,6 +1,7 @@
 package repositories_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -42,6 +43,22 @@ func TestUpdateProduct(t *testing.T) {
 		}
 		if products.Name != "New Product  1" {
 			t.Errorf("Product name should be equal to 'New Product  1' but have %v", products.Name)
+		}
+	})
+
+	t.Run("Update Product NOK", func(t *testing.T) {
+		var ID uint = 1
+		product := &nmodel.Product{
+			Name: "New Product  1",
+		}
+
+		mock.ExpectBegin()
+		mock.ExpectExec("UPDATE").WillReturnError(errors.New("Update product fail"))
+		mock.ExpectCommit()
+
+		_, err := repository.UpdateProduct(ID, product)
+		if err == nil {
+			t.Errorf("This should be an error")
 		}
 	})
 }
